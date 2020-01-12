c15a0c4fc47abd8eff17b89dc143b2bbbdeed2a2
paphonb <paphonb@gmail.com>
2020-01-13 03:16:54 +0700

    Click logo to go home

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useCallback } from "react"
+import { useHistory } from "react-router-dom"
 
 function ContentCard({ children, ...rest }) {
+  const history = useHistory()
+  const handleClick = useCallback(() => {
+    history.push('/')
+  }, [history])
   return (
     <div>
-      <div className="logo-small" />
+      <div className="logo-small" onClick={handleClick} />
       <div className="content-card" {...rest}>
         {children}
       </div>
