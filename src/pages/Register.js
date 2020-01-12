f0513a9f55aa5a38473d2e2764929c16b15ccd66
paphonb <paphonb@gmail.com>
2020-01-12 22:28:17 +0700

    Show faculty number only in form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -129,7 +129,7 @@ function Register() {
                     option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
                   }>
                   {facultyCodes.map(code => <Option key={code} value={code}>
-                    {t('facultyNames.' + code)}
+                    {`${code} ${t(`facultyNames.${code}`)}`}
                   </Option>)}
                 </Select>
               }
