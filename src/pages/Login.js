98c922c0672094a843e0adbd675a123e0dbc7bd8
paphonb <paphonb@gmail.com>
2020-01-13 20:30:33 +0700

    Fix code style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,39 +19,37 @@ import { telPattern } from "../utils"
 
 
 const validationSchema = yup.object().shape({
-    ID: yup
+  ID: yup
       .string()
       .nationalId("invalidValue")
       .required(),
-    tel: yup
+  tel: yup
       .string()
       .matches(telPattern, "invalidValue")
       .required(),
-  })
+})
 
 function Login(){
-    const { t } = useTranslation()
-    const methods = useForm( { validationSchema })
-    const { getValues, handleSubmit } = methods
+  const { t } = useTranslation()
+  const methods = useForm( { validationSchema })
 
-    return (
-        <FormContext {...methods}>
-            <ContentCard id="login-form">
-                <div id="title-container">
-                    <p className="title" > { t("login.title") }</p>
-                    <p className="subtitle">{ t("login.subtitle") }</p>
-                </div>
-                <Form layout="vertical"  >
-                    <Field name="ID" title = { t("idNumber") } pattern="\d*" />
-                    <Field name="tel" title = { t("phoneNumber") } type="tel"/>
-                    <p id="check-button">
-                        <Button type="submit" > { t("login.submit") } </Button>
-                    </p>
-                </Form>
-            </ContentCard>
-        </FormContext>
-    )
-
+  return (
+    <FormContext {...methods}>
+      <ContentCard id="login-form">
+        <div id="title-container">
+          <p className="title">{t("login.title")}</p>
+          <p className="subtitle">{t("login.subtitle")}</p>
+        </div>
+        <Form layout="vertical"  >
+          <Field name="ID" title={t("idNumber")} pattern="\d*" />
+          <Field name="tel" title={t("phoneNumber")} type="tel"/>
+          <p id="check-button">
+            <Button type="submit">{t("login.submit")}</Button>
+          </p>
+        </Form>
+      </ContentCard>
+    </FormContext>
+  )
 }
 
 export default Login
