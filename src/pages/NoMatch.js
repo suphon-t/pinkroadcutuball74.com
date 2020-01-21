be2801a00f62ad5960a03dd059dafd2c924fc4c5
paphonb <paphonb@gmail.com>
2020-01-21 23:21:27 +0700

    Modify 404 page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -2,20 +2,24 @@ import React from "react"
 import ContentCard from "../components/ContentCard"
 import { useTranslation } from "react-i18next"
 import { Link } from "react-router-dom"
+import { Result } from "antd"
 
-import Title from "../components/Title"
-import Subtitle from "../components/Subtitle"
 import OrangeButton from "../components/OrangeButton"
 
 function NoMatch() {
   const { t } = useTranslation()
   return (
-    <ContentCard style={{ textAlign: 'center' }}>
-      <Title>{t('noMatch.title')}</Title>
-      <Subtitle>{t('noMatch.subtitle')}</Subtitle>
-      <Link style={{ display: 'inline-block', marginTop: 16 }} to="/">
-        <OrangeButton>{t('noMatch.returnHome')}</OrangeButton>
-      </Link>
+    <ContentCard>
+      <Result
+        status="404"
+        title="404"
+        subTitle={t('noMatch.subtitle')}
+        extra={
+          <Link style={{ display: 'inline-block' }} to="/">
+            <OrangeButton>{t('noMatch.returnHome')}</OrangeButton>
+          </Link>
+        }
+      />
     </ContentCard>
   )
 }
