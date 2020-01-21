ed810f94d57bfb9a6ec23c5f5957dc3ac3f78fac
cyredhat <cyredhat@gmail.com>
2020-01-21 15:41:36 +0700

    Update ticket page

--- src/pages/GetTicket.js
+++ src/pages/GetTicket.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react"
 import ContentCard from "../components/ContentCard"
 import Ticket from "../components/Ticket"
+import OrangeButton from "../components/OrangeButton"
+import styled from "styled-components"
+import vars from "../styles/vars"
+import {lighten, darken} from "polished"
 import { Input } from "antd"
+
+const LogoutButton = styled(OrangeButton)`
+  background: ${vars.darkBlue};
+  border: 1px solid ${vars.darkBlue};
+  color: ${vars.orange};
+  margin: 40px auto;
+  padding: 18px 0;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 0px;
+  &:not(:disabled) {
+    &:hover,
+    &:focus {
+      background: ${lighten(0.08, vars.darkBlue)};
+    }
+    &:active {
+      background: ${darken(0.1, vars.darkBlue)}
+    }
+  }
+`
 
 function GetTicket() {
   const [number, setNumber] = useState('0074')
@@ -11,9 +35,10 @@ function GetTicket() {
     <>
       <Input value={number} onChange={e => setNumber(e.target.value)} />
       <Input value={name} onChange={e => setName(e.target.value)} />
-      <ContentCard style={{ padding: 0 }}>
+      <ContentCard style={{ padding: 16 }}>
         <Ticket data={data} style={{ borderRadius: 10 }} />
       </ContentCard>
+      <LogoutButton>ออกจากระบบ</LogoutButton>
     </>
   )
 }
