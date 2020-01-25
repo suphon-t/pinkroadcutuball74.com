368d5df8be55626c6689821b4a0ee815340ad2e5
paphonb <paphonb@gmail.com>
2020-01-25 11:42:11 +0700

    Logout on token expire

--- src/auth/AuthProvider.js
+++ src/auth/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState, useCallback, useMemo } from "react"
 
 import { parseToken } from "../utils"
+import moment from "moment"
 
 export const AuthContext = createContext()
 
@@ -15,19 +16,23 @@ function AuthProvider(props) {
   const isAdmin = role === 'admin'
   const isStaff = isAdmin || role === 'staff'
 
-  // Persist the token
-  useEffect(() => {
-    if (token === null) {
-      localStorage.removeItem('access_token')
-      localStorage.removeItem('ticket')
-    } else {
-      localStorage.setItem('access_token', token)
-    }
-  }, [token])
-
   // Callback functions
   const login = useCallback(token => setToken(token), [])
   const logout = useCallback(() => setToken(null), [])
+
+  // Validate and persist the token
+  useEffect(() => {
+    if (tokenData) {
+      if (moment().isAfter(moment(tokenData.exp * 1000))) {
+        logout()
+      } else {
+        localStorage.setItem('access_token', token)
+      }
+    } else {
+      localStorage.removeItem('access_token')
+      localStorage.removeItem('ticket')
+    }
+  }, [token, tokenData, logout])
 
   const value = { isAuthenticated, isAdmin, isStaff, role, token, userId, login, logout }
 
